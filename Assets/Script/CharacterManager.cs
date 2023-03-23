cb68cc2b5f034ce81cf9ba4eefd59cb89585d8ce
ingan1357 <67782455+ingan1357@users.noreply.github.com>
2023-03-23 23:47:35 +0900

    interactive UI

---
+++ Assets/Script/CharacterManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public GameObject PlayerData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set player data
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Dialogue.cs
+++ Assets/Script/Dialogue.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,10 +14,5 @@ public class test : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void test11()
-    {
-
     }
 }
