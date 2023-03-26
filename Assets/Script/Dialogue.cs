59dfc040fa472f0edc2d319d3013b017fe54435c
ingan1357 <67782455+ingan1357@users.noreply.github.com>
2023-03-26 20:20:04 +0900

    add typing effect

--- Assets/Script/Dialogue.cs
+++ Assets/Script/Dialogue.cs
@@ -2,19 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Dialogue : MonoBehaviour
 {
-    public string[] Dialogue;
-    public Text TextDisplay;
+    public string[] _Dialogue;
+    public TextMeshProUGUI TextDisplay;
+    public float TypingSpeed = 0.15f;
+
     void Start()
     {
-        
+        TextDisplay.text = "aaa";
     }
 
-    
+
     void Update()
     {
-        
+
+    }
+
+    //typing effect
+    IEnumerator TypingEffect()
+    {
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i <= _Dialogue.Length; i++)
+        {
+            TextDisplay.text = _Dialogue[i].Substring(0, i);
+
+            yield return new WaitForSeconds(TypingSpeed);
+        }
     }
 }
